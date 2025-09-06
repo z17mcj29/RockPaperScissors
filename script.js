26a68b8129609d4fa4a42d1a0424e79567aa88a1
Michael Johnson <z17.mcj@gmail.com>
2025-09-06 09:32:57 -0600

    Final commit to complete the assignment!

--- script.js
+++ script.js
@@ -30,25 +30,44 @@ function getHumanChoice(){
 }
 //console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-
-    humanChoice = humanChoice.toLowerCase();
-
-    if(computerChoice === humanChoice){
-        return `Human chose ${humanChoice} and Computer chose ${computerChoice} so its a tie. Try again!`;
-    }  else if( humanChoice === "rock" &&     computerChoice === "scissors" || 
-                humanChoice === "paper" &&    computerChoice === "rock" ||
-                humanChoice === "scissors" && computerChoice === "paper"){
-            humanScore += 1;
-            return `Human chose ${humanChoice} and Computer chose ${computerChoice}. You win this round!`;
-    } else{
-        computerScore += 1;
-        return `Human chose ${humanChoice} and Computer chose ${computerChoice}. You lose this round!` 
-    }
-}
+/*
 console.log(playRound(getHumanChoice(), getComputerChoice()));
 console.log(humanScore);
 console.log(computerScore);
+*/
+
+function playGame(rounds){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+
+        humanChoice = humanChoice.toLowerCase();
+
+        if(computerChoice === humanChoice){
+            return console.log(`Human chose ${humanChoice} and Computer chose ${computerChoice} so its a tie. Try again!`);
+        }  else if( humanChoice === "rock" &&     computerChoice === "scissors" || 
+                    humanChoice === "paper" &&    computerChoice === "rock" ||
+                    humanChoice === "scissors" && computerChoice === "paper"){
+                humanScore += 1;
+                return console.log(`Human chose ${humanChoice} and Computer chose ${computerChoice}. You win this round!`);
+        } else{
+            computerScore += 1;
+            return console.log(`Human chose ${humanChoice} and Computer chose ${computerChoice}. You lose this round!`); 
+        }        
+}
+    for(let i = 0; i < rounds; i++){
+        playRound(getHumanChoice(), getComputerChoice())
+        
+        console.log(`The Current Score is human ${humanScore} and computer ${computerScore}`)
+    }
+    if(humanScore === computerScore){
+        return console.log(`Close contest! You tied with a score of ${humanScore} to ${computerScore}`);
+    } else if(humanScore > computerScore){
+        return console.log(`Nice job Human! You won with a score of ${humanScore} to ${computerScore}`);
+    } else{
+        return console.log(`Nice try Human! You tried your best but lost with a score of ${humanScore} to ${computerScore}`);
+    }
+    
+}
+playGame(5);
